1a3db077157a05c57a17ab79ccbc5ba81ca88330
hudaaddelson@gmail.com <hudaaddelson@gmail.com>
2022-06-17 20:45:28 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 16,
+    centeredSlides: true,
+    slidesPerview: 'auto',
+    loop: 'true',
+});
+
+// Skils Bar
+// $(window).scroll(function(){
+//     let hT = $("#skils").offset().top,
+//     hH = $("$skils").outerHeight(),
+//     wH = $(window).height(),
+//     wS = $(this).scrollTop();
+//     if(wS > (hT+hH-1.4*wH)){
+//         JQuery(".skilbar").each(funtion(){
+//             JQuery(this).find(".skils").animate({
+
+//             })
+//         })
+//     }
+// })
