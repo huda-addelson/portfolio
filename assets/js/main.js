5a5bcbe079f75c0d72b7c071a405a7413ab584fb
hudaaddelson@gmail.com <hudaaddelson@gmail.com>
2022-06-26 19:10:29 +0700

    update feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,4 @@
 
-// let homeSwiper = new Swiper(".home-swiper", {
-//     spaceBetween: 16,
-//     centeredSlides: true,
-//     slidesPerview: 'auto',
-//     loop: 'true',
-// });
-
-// Skils Bar
-// $(window).scroll(function(){
-//     let hT = $("#skils").offset().top,
-//     hH = $("$skils").outerHeight(),
-//     wH = $(window).height(),
-//     wS = $(this).scrollTop();
-//     if(wS > (hT+hH-1.4*wH)){
-//         JQuery(".skilbar").each(funtion(){
-//             JQuery(this).find(".skils").animate({
-
-//             })
-//         })
-//     }
-// })
-/* =============== HEADER STICKY =============== */
-const header = document.getElementById("header");
-
-function stickyNavbar() {
-    header.classList.toggle("scrolled", window.pageYOffset > 0);
-}
-
-stickyNavbar();
-window.addEventListener("scroll", stickyNavbar);
-
 /* =============== TAMPIL-MENU =============== */
 const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
@@ -48,3 +17,14 @@ if (navClose) {
         navMenu.classList.remove("show-menu");
     })
 }
+/* =============== HEADER STICKY =============== */
+
+
+function scrolHeader() {
+    const header = document.getElementById("header");
+    if (this.scrollY >= 50) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrolHeader);
+
+
